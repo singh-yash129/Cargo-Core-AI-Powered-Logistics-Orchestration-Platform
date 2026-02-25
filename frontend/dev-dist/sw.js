d1d558c49225b41dc807704ccc3290db54f8fc9f
singh-yash129 <singhyyash129@gmail.com>
2026-02-25 19:34:12 +0530

    feat: add Workbox-generated service worker for caching and offline support.

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3tl8ofj9f2o"
+    "revision": "0.p8kprpqeih8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
