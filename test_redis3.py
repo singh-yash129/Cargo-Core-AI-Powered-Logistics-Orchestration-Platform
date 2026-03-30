5b245c18b8b9ab42128e2e98fa7035b02c171bcc
Pruthvi Prasad <pruthviprasad280@gmail.com>
2026-03-30 19:46:44 +0530

    chore: remove tracked logs and local junk files

--- test_redis3.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-from redis.asyncio import from_url
-async def main():
-    r = from_url('redis://localhost:6379/0', decode_responses=True)
-    print(await r.get('signup_otp:pruthviprasads14@gmail.com'))
-asyncio.run(main())
